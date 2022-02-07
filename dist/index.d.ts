f2793cb337f72f0fc4c87ff6172379decd224ebe
Vijay <ossdeveloper8@gmail.com>
2022-02-07 23:55:28 +0530

    Completed till Simple Moving Average.
    updated README.md
    
    Signed-off-by: Vijay <ossdeveloper8@gmail.com>

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,5 @@
-export {};
+export { MathOps } from "./base/MathOps";
+export { Precision } from "./base/Precision";
+export { SMA } from "./averages/SMA";
+export declare class Tsti {
+}
--- dist/index.js
+++ dist/index.js
@@ -1,22 +1,23 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const MathOps_1 = __importDefault(require("./base/MathOps"));
-const Precision_1 = __importDefault(require("./base/Precision"));
-const SMA_1 = __importDefault(require("./averages/SMA"));
+exports.Tsti = exports.SMA = exports.Precision = exports.MathOps = void 0;
+var MathOps_1 = require("./base/MathOps");
+Object.defineProperty(exports, "MathOps", { enumerable: true, get: function () { return MathOps_1.MathOps; } });
+var Precision_1 = require("./base/Precision");
+Object.defineProperty(exports, "Precision", { enumerable: true, get: function () { return Precision_1.Precision; } });
+var SMA_1 = require("./averages/SMA");
+Object.defineProperty(exports, "SMA", { enumerable: true, get: function () { return SMA_1.SMA; } });
 class Tsti {
 }
-const tsti = new Tsti();
-const prec = 4;
-const precision = new Precision_1.default(prec);
-const mathOps = new MathOps_1.default(prec);
-let random = 10.353456789;
-console.log(`10.353456789 sanitized to ${precision.sanitize(random)}`);
-console.log(`Add - `, mathOps.add([5.16284263, 1.62531289]));
-console.log(`Subtraction - `, mathOps.subtract(5.16284263, 1.62531289));
-console.log(`Multiply - `, mathOps.multiply([5.16284263, 1.62531289]));
-console.log(`Division - `, mathOps.divide(5.16284263, 1.62531289));
-const sma = new SMA_1.default([5.16284263, 1.62531289, 5.16284263], prec).sma;
-console.log(`SMA - `, sma);
+exports.Tsti = Tsti;
+// const prec = 4;
+// const precision = new Precision(prec);
+// const mathOps = new MathOps(prec);
+// let random = 10.353456789;
+// console.log(`10.353456789 sanitized to ${precision.sanitize(random)}`);
+// console.log(`Add - `,mathOps.add([5.16284263,1.62531289]));
+// console.log(`Subtraction - `,mathOps.subtract(5.16284263,1.62531289));
+// console.log(`Multiply - `, mathOps.multiply([5.16284263,1.62531289]));
+// console.log(`Division - `, mathOps.divide(5.16284263,1.62531289));
+// const sma = new SMA([5.16284263,1.62531289,5.16284263],prec).sma;
+// console.log(`SMA - `, sma);
